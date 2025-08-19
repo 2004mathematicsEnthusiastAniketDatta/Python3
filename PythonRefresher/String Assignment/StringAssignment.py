7146f7deb2acfdd43566bf206411560582854be8
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2025-08-19 08:56:59 +0000

    Assignment

--- PythonRefresher/String Assignment/StringAssignment.py
+++ PythonRefresher/String Assignment/StringAssignment.py
@@ -9,7 +9,28 @@ Weeks is = 7 days
 decimals within the return is allowed..
 '''
 
+class BirthdayCalculator:
+    def __init__(self):
+        self.days_per_week = 7
+    
+    def get_days_input(self):
+        """Get the number of days from user input"""
+        return int(input("How many days until your birthday? "))
+    
+    def calculate_weeks(self, days):
+        """Calculate weeks from days"""
+        return round(days / self.days_per_week, 2)
+    
+    def display_result(self, weeks):
+        """Display the result"""
+        print(weeks)
+    
+    def run(self):
+        """Main method to run the birthday calculator"""
+        days = self.get_days_input()
+        weeks = self.calculate_weeks(days)
+        self.display_result(weeks)
 
-days = int(input("How many days until your birthday? "))
-
-print(round(days/7, 2))
+# Create an instance and run the calculator
+calculator = BirthdayCalculator()
+calculator.run()
