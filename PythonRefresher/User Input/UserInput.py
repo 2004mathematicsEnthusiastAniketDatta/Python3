23fa5ae641c86bbc4f2a63cd8882b1f1010acc64
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2025-08-19 08:31:00 +0000

    userInput

--- PythonRefresher/User Input/UserInput.py
+++ PythonRefresher/User Input/UserInput.py
@@ -1,3 +1,5 @@
+import sys
+
 """
 User Input
 """
@@ -7,5 +9,19 @@ days = input("How many days before your birthday: ")
 print(f"Hi {first_name}, only {days} days "
       f"before your birthday!")
 
+class UserInput:
+       def userInput(self):
+                  # Read from stdin character by character
+                  input_chars = []
+                  while True:
+                        char = sys.stdin.read(1)
+                        if char == '\n' or char == '':
+                              break
+                        input_chars.append(char)
+                  return ''.join(input_chars)
+
+
+n=UserInput().userInput()
+print(f"You entered: {n}")
 
 
